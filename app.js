a536e28a0e2f9f4b421fe9fa24d3d1365e458692
Snehal2020 <snehallande2020@gmail.com>
2024-08-14 21:30:20 +0530

    latest commit

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app=express()
 app.use("/files",express.static("files"))
 require('dotenv').config() 
 require('./db');
-const Pdf=require('./models/pdf')
-const pdfSchema=mongoose.model('Pdf_s')
+
 const authRoute=(require('./routes/authRoute'))
 const departmentRoutes =require('./routes/department')
 const courseRoutes =require('./routes/course')
@@ -22,95 +21,13 @@ const cors=require('cors')
 app.use(cors());
 const {objectIdcontroller} = require('./controllers/courseController')
 const {objectIdcontroller_f} = require('./controllers/facultyController')
-const multer  = require('multer')
-// const upload=multer({ dest: './files' })
+app.use(express.static(path.join(__dirname,'./client/build')))
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   next();
-// });
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+  })
 
-// Your route definitions here
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './files')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now()
-    cb(null, uniqueSuffix+file.originalname)
-  }
-})
-
- const upload= multer({ storage: storage })
-app.get('/get_pdf', async(req,res)=>{
-  try {
-    await pdfSchema.find({}).then((data)=>{
-        res.send({status:"ok", data:data})
-    })
-  } catch (error) {
-    res.send(error)
-  }
-  
-})
-app.post('/upload_pdf', upload.single("file"),async(req, res)=>{
-      console.log(req.file)
-      
-      try {
-        await pdfSchema.create({pdf:req.file.filename})
-        res.send('Okkkkkkkkkkk')
-      } catch (error) {
-        res.send(error)
-      }
-
-      
-    })
-  
-    
-
-    // app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); 
-    // Serve static files from /uploads
-
-// Multer configuration for file uploads
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'uploads/');
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// // Route to handle file upload
-// app.post('/upload_pdf', upload.single('file'), async (req, res) => {
-//   try {
-//     const newPdf = new Pdf({
-//       title: req.body.title,
-//       pdfPath: req.file.path,
-//     });
-//     await newPdf.save();
-//     res.status(201).send('PDF Uploaded');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// // Route to get list of PDFs
-// app.get('/get_pdfs', async (req, res) => {
-//   try {
-//     const pdfs = await Pdf.find();
-//     res.status(200).json(pdfs);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server Error');
-//   }
-// });
-  
 app.post('/rec',objectIdcontroller);
 app.post('/dept-id',objectIdcontroller_f);
   app.listen(process.env.PORT, () => {
